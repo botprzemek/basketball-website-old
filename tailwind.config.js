e7bee85ec6334b011db679b650dd864edb303e0d
WizTV <pawel.wiezik@gmail.com>
2023-02-21 20:07:07 +0100

    baner-~~changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./public/*.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'main': 'rgb(235, 64, 52)',
-        'secd': 'rgb(255, 255, 255)',
-        'bkgd': 'rgb(29, 29, 29)',
-      },
-      backgroundSize: {
-        full: '400%, 400%',
-      },
-      keyframes: {
-        gradient: {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-        }
-      },
-      animation: {
-        gradient: 'gradient 30s ease infinite',
-      },
-    },
-  },
-}
+	content: [
+		"./index.html",
+		"./public/*.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			// backgroundImage: {
+			// 	"banner-pattern": "url('assets/baner.svg')",
+			// },
+			colors: {
+				main: "rgb(235, 64, 52)",
+				secd: "rgb(255, 255, 255)",
+				bkgd: "rgb(29, 29, 29)",
+			},
+			backgroundSize: {
+				full: "400%, 400%",
+			},
+			keyframes: {
+				gradient: {
+					"0%, 100%": { backgroundPosition: "0% 50%" },
+					"50%": { backgroundPosition: "100% 50%" },
+				},
+			},
+			animation: {
+				gradient: "gradient 30s ease infinite",
+			},
+		},
+	},
+};
