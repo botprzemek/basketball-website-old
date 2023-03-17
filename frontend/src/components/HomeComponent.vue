30785cd936aab963504d0b0bbe16ca9c47c11d81
botprzemek <botprzemekk@gmail.com>
2023-03-17 02:29:19 +0100

    First backend

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -1,7 +1,7 @@
 <template>
       <main class="z-10 w-screen h-screen relative grid place-content-center select-none">
         <section class="grid place-items-center overflow-hidden">
-          <img src="@/assets/logo.svg" height="230" width="230">
+          <img src="@/assets/logo.svg" height="230" width="230" alt="Logo">
           <h1 class="heading -translate-y-10 opacity-0 transform-gpu transition-all ease-out duration-700 text-white font-bold text-6xl text-center uppercase pt-10 mb-1">Knury Knurów</h1>
           <p class="description opacity-0 transition-all ease-out duration-1000 rounded-xl backdrop-blur-md bg-white/10 text-white text-md text-center mb-4 py-2 w-full">Strona jest w trakcie tworzenia.</p>
           <a href="https://zrzutka.knuryknurow.pl/" target="_blank" class="button scale-0 opacity-0 transform-gpu transition-all ease-out duration-1000 backdrop-blur-md bg-white/10 py-2 px-3 rounded-full flex col-auto items-center w-fit">
