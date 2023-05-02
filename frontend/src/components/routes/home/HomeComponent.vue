8f0b03e3546e72fc3ba790b8ad05fdcbcb191c86
botprzemek <92102878+botprzemek@users.noreply.github.com>
2023-05-02 22:50:26 +0200

    Responsive events component

--- frontend/src/components/routes/home/HomeComponent.vue
+++ frontend/src/components/routes/home/HomeComponent.vue
@@ -1,6 +1,12 @@
 <template>
-  <section class="h-full w-full grid place-content-center select-none grid place-items-center bg-secd">
-    <!--    <img src="@/assets/knury_logo.svg" class="aspect-square h-[50vh]" alt="Logo">-->
+  <section class="z-10 w-full h-full relative place-content-center select-none mx-4 mb-48">
+    <article class="w-full h-full grid">
+      <img src="@/assets/knury_logo.svg" class="aspect-square w-2/3 sm:w-1/4" alt="Logo">
+      <h1 class="text-4xl sm:text-8xl font-bold">Knury Knurów</h1>
+      <p class="text-2xl">
+        Witaj na naszej stronie!
+      </p>
+    </article>
   </section>
 </template>
 
