7f4aaa6a4c0ce161011847cf79421ad046c97f13
botprzemek <botprzemekk@gmail.com>
2023-05-03 11:09:10 +0200

    Backend fix

--- backend/methods/mail/Mail.js
+++ backend/methods/mail/Mail.js
@@ -5,7 +5,7 @@ export const sendMail = async (recevier, token) => {
     await transporter.sendMail({
         from: '"Knury Knurów" <info@knuryknurow.pl>',
         to: recevier,
-        subject: 'Potwierdzenie rejestracji na turniej Knury Knurów 2023 🏀🔥',
+        subject: 'Potwierdzenie rejestracji na turniej Knury Streetball 2023 🏀🔥',
         html: getTemplate(token),
     });
     console.log(`Send email to ${recevier} with ${token}`);
