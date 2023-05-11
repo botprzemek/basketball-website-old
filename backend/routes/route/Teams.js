ad097b93e70bbcea4c928475b0fd6f4108bff66d
botprzemek <92102878+botprzemek@users.noreply.github.com>
2023-05-11 21:37:54 +0200

    Backend fix

--- backend/routes/route/Teams.js
+++ backend/routes/route/Teams.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
-import {getAmount} from '../../methods/firebase/firestore/Get.js';
+import {getAmountWhere} from '../../methods/firebase/firestore/Get.js';
 
 const router = Router();
 
 //router.get('/', async (request, response) => response.send(await getData('register', null)));
 //router.get('/:team', async (request, response) => response.send(await getData('register', request.params.team)));
 router.get('/amount/:category', async (request, response) => {
-    response.send({amount:await getAmount('register', 'category', '==', 1*request.body.params.category)});
+    response.send({amount:await getAmountWhere('register', 'category', '==', 1*request.body.params.category)});
 });
 
 export default router;
